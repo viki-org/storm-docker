a62d61c39e647fa1aee79dce1abe958ce3cda624
Pang Yan Han <yanhan@viki.com>
2014-04-17 14:33:00 +0800

    Add changes made by Neerav, Ishan and myself
    These include
    - exposing port 22 for storm-supervisor (so we can ssh into it)
    - for the Dockerfile for storm/ , change the root password, set the
      STORM_HOME environment variable, add an ssh supervisor file
    - revert wurstmeister's changes to use storm 0.9.1 , and use storm
      0.9.0.1 instead, which is the version we've been using all the while
    
    Signed-off-by: Pang Yan Han <yanhan@viki.com>

--- start-storm.sh
+++ start-storm.sh
@@ -11,5 +11,5 @@ else
 fi
 
 docker run -p 49773:3773 -p 49772:3772 -p 49627:6627 --name nimbus --link zookeeper:zk -h nimbus -d wurstmeister/storm-nimbus 
-docker run -p 49000:8000 --name supervisor --link nimbus:nimbus --link zookeeper:zk -h supervisor -d wurstmeister/storm-supervisor
+docker run -p 49000:8000 -p 49022:22 --name supervisor --link nimbus:nimbus --link zookeeper:zk -h supervisor -d wurstmeister/storm-supervisor
 docker run -p 49080:8080 --name ui --link nimbus:nimbus --link zookeeper:zk -d wurstmeister/storm-ui
