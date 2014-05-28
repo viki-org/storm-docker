761c658690820ff6c5b33ffedeb027731eb75bc2
Pang Yan Han <yanhan@viki.com>
2014-05-28 14:08:59 +0800

    Use standard ports for zookeeper, nimbus, drpc
    This is to simplify things when we have a multiple zookeeper setup in
    the near future.
    
    Signed-off-by: pang Yan Han <yanhan@viki.com>

--- start-storm.sh
+++ start-storm.sh
@@ -7,9 +7,9 @@ then
     echo "Zookeeper is already running"
 else
     echo "Starting Zookeeper"
-    docker run -p 49181:2181  -h zookeeper --name zookeeper -d viki_data/zookeeper
+    docker run -p 2181:2181  -h zookeeper --name zookeeper -d viki_data/zookeeper
 fi
 
-docker run -p 49773:3773 -p 49772:3772 -p 49627:6627 --name nimbus --link zookeeper:zk -h nimbus -d viki_data/storm-nimbus
+docker run -p 3773:3773 -p 3772:3772 -p 6627:6627 --name nimbus --link zookeeper:zk -h nimbus -d viki_data/storm-nimbus
 docker run -p 49080:8080 --name ui --link nimbus:nimbus --link zookeeper:zk -d viki_data/storm-ui
 ./run-storm-supervisor.sh
