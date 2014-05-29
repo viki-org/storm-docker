83d3d9bf0e40b29d0c1b26d20f7fd82f85f4982f
Pang Yan Han <yanhan@viki.com>
2014-05-29 11:00:44 +0800

    Shift run-storm-*.sh scripts to `scripts` folder
    To clear up some clutter in the top level of the repository.
    
    Signed-off-by: Pang Yan Han <yanhan@viki.com>

--- start-storm.sh
+++ start-storm.sh
@@ -10,9 +10,9 @@ else
     docker run -p 2181:2181  -h zookeeper --name zookeeper -d viki_data/zookeeper
 fi
 
-./run-storm-docker-component.sh -p 3773:3773 -p 3772:3772 -p 6627:6627 --name nimbus --link zookeeper:zk -h nimbus -d viki_data/storm-nimbus
-./run-storm-docker-component.sh -p 49080:8080 --name ui --link nimbus:nimbus --link zookeeper:zk -d viki_data/storm-ui
-./run-storm-supervisor.sh \
+scripts/run-storm-docker-component.sh -p 3773:3773 -p 3772:3772 -p 6627:6627 --name nimbus --link zookeeper:zk -h nimbus -d viki_data/storm-nimbus
+scripts/run-storm-docker-component.sh -p 49080:8080 --name ui --link nimbus:nimbus --link zookeeper:zk -d viki_data/storm-ui
+scripts/run-storm-supervisor.sh \
   --dns 127.0.0.1 --dns 8.8.8.8 --dns 8.8.8.4 \
   -p 49000:8000 -p 127.0.0.1:49022:22 \
   -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 \
