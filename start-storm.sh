9d3ac4c5a9ae3d6791981aeec030be0484e7b830
Pang Yan Han <yanhan@viki.com>
2014-05-29 13:39:17 +0800

    start-storm.sh: Fix indentation and spacing
    Signed-off-by: Pang Yan Han <yanhan@viki.com>

--- start-storm.sh
+++ start-storm.sh
@@ -4,14 +4,25 @@ ZOOKEEPER=`docker ps -a | awk '{print $NF}'  | grep "zookeeper$"`
 ZOOKEEPER_RUNNING=$?
 if [ $ZOOKEEPER_RUNNING -eq 0 ] ;
 then
-    echo "Zookeeper is already running"
+  echo "Zookeeper is already running"
 else
-    echo "Starting Zookeeper"
-    docker run -p 2181:2181  -h zookeeper --name zookeeper -d viki_data/zookeeper
+  echo "Starting Zookeeper"
+  docker run -p 2181:2181 -h zookeeper --name zookeeper -d viki_data/zookeeper
 fi
 
-scripts/run-storm-docker-component.sh -p 3773:3773 -p 3772:3772 -p 6627:6627 --name nimbus --link zookeeper:zk -h nimbus -d viki_data/storm-nimbus
-scripts/run-storm-docker-component.sh -p 49080:8080 --name ui --link nimbus:nimbus --link zookeeper:zk -d viki_data/storm-ui
+scripts/run-storm-docker-component.sh \
+  -p 3773:3773 -p 3772:3772 -p 6627:6627 \
+  --name nimbus \
+  --link zookeeper:zk \
+  -h nimbus \
+  -d viki_data/storm-nimbus
+
+scripts/run-storm-docker-component.sh \
+  -p 49080:8080 \
+  --name ui \
+  --link nimbus:nimbus --link zookeeper:zk \
+  -d viki_data/storm-ui
+
 scripts/run-storm-supervisor.sh \
   --dns 127.0.0.1 --dns 8.8.8.8 --dns 8.8.8.4 \
   -p 49000:8000 -p 127.0.0.1:49022:22 \
