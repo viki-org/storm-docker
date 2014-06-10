36bcd0ed3a5addb22a29f4c82c05ae4c1262b83b
Pang Yan Han <yanhan@viki.com>
2014-06-10 18:58:06 +0800

    Make `start-storm.sh` more modular
    We refactor the code to create a new function `start_storm_docker`,
    which contains a `case` statement that dispatches on the Storm
    components. This allows a user to use the script for selected components
    instead of starting everything.
    
    Signed-off-by: Pang Yan Han <yanhan@viki.com>

--- start-storm.sh
+++ start-storm.sh
@@ -1,34 +1,85 @@
 #!/bin/bash
 
-ZOOKEEPER=`docker ps -a | awk '{print $NF}'  | grep "zookeeper$"`
-ZOOKEEPER_RUNNING=$?
-if [ $ZOOKEEPER_RUNNING -eq 0 ] ;
+# This is used to run a Docker container in this repository after you have
+# run the `make` command to build the Docker images.
+
+# Inspects the `IMAGE` column of the output of `docker ps` to see if the given
+# Docker container is running
+function is_docker_container_running {
+  docker ps | tail -n+2 | awk '{print $2}' | grep --quiet $1
+}
+
+# Starts a given storm-docker Docker container
+function start_storm_docker {
+  case $1 in
+  nimbus)
+    if is_docker_container_running "viki_data/storm-nimbus"
+    then
+      echo "storm nimbus Docker container already running"
+    else
+      SKIP_PIP_INSTALL=$SKIP_PIP_INSTALL scripts/run-storm-docker-component.sh \
+        --name nimbus \
+        --link zookeeper:zk \
+        -h nimbus \
+        -d viki_data/storm-nimbus \
+        --storm-docker-component nimbus \
+        --storm-docker-component drpc
+    fi
+    ;;
+  supervisor)
+    if is_docker_container_running "viki_data/storm-supervisor"
+    then
+      echo "storm supervisor Docker container already running"
+    else
+      SKIP_PIP_INSTALL=$SKIP_PIP_INSTALL scripts/run-storm-supervisor.sh \
+        --dns 127.0.0.1 --dns 8.8.8.8 --dns 8.8.8.4 \
+        -p 127.0.0.1:49022:22 \
+        --name supervisor \
+        -d viki_data/storm-supervisor
+    fi
+    ;;
+  ui)
+    if is_docker_container_running "viki_data/storm-ui"
+    then
+      echo "storm ui Docker container already running"
+    else
+      SKIP_PIP_INSTALL=$SKIP_PIP_INSTALL scripts/run-storm-docker-component.sh \
+        --name ui \
+        --link nimbus:nimbus --link zookeeper:zk \
+        -d viki_data/storm-ui \
+        --storm-docker-component ui
+    fi
+    ;;
+  zookeeper)
+    if is_docker_container_running "viki_data/zookeeper"
+    then
+      echo "zookeeper Docker container already running"
+    else
+      SKIP_PIP_INSTALL=$SKIP_PIP_INSTALL scripts/run-zookeeper.sh \
+        -p 127.0.0.1:49122:22 \
+        -h zookeeper --name zookeeper \
+        -d viki_data/zookeeper
+    fi
+    ;;
+  *)
+    echo "Unknown storm-docker component: \"$1\""
+    ;;
+  esac
+}
+
+# Execution of this bash script starts from here
+if [ $# -eq 0 ]
 then
-  echo "Zookeeper is already running"
+  # No arguments supplied to this script; run every component
+  start_storm_docker "zookeeper"
+  start_storm_docker "nimbus"
+  start_storm_docker "ui"
+  start_storm_docker "supervisor"
 else
-  echo "Starting Zookeeper"
-  SKIP_PIP_INSTALL=$SKIP_PIP_INSTALL scripts/run-zookeeper.sh \
-    -p 127.0.0.1:49122:22 \
-    -h zookeeper --name zookeeper \
-    -d viki_data/zookeeper
+  # At least one argument was supplied to this script.
+  # We start each Docker container in the arguments.
+  for component in "$@"
+  do
+    start_storm_docker $component
+  done
 fi
-
-SKIP_PIP_INSTALL=$SKIP_PIP_INSTALL scripts/run-storm-docker-component.sh \
-  --name nimbus \
-  --link zookeeper:zk \
-  -h nimbus \
-  -d viki_data/storm-nimbus \
-  --storm-docker-component nimbus \
-  --storm-docker-component drpc
-
-SKIP_PIP_INSTALL=$SKIP_PIP_INSTALL scripts/run-storm-docker-component.sh \
-  --name ui \
-  --link nimbus:nimbus --link zookeeper:zk \
-  -d viki_data/storm-ui \
-  --storm-docker-component ui
-
-SKIP_PIP_INSTALL=$SKIP_PIP_INSTALL scripts/run-storm-supervisor.sh \
-  --dns 127.0.0.1 --dns 8.8.8.8 --dns 8.8.8.4 \
-  -p 127.0.0.1:49022:22 \
-  --name supervisor \
-  -d viki_data/storm-supervisor
