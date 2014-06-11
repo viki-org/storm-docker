6ee19a94aff6eea6b50338fee42ad422ecba5d8b
Pang Yan Han <yanhan@viki.com>
2014-06-11 17:05:41 +0800

    Modularize `destroy-storm.sh` script
    Similar to the last patch, we modify the `destroy-storm.sh` script to
    allow the user to choose the containers to stop.
    
    Signed-off-by: Pang Yan Han <yanhan@viki.com>

--- destroy-storm.sh
+++ destroy-storm.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-docker stop ui; docker rm ui
-docker stop supervisor; docker rm supervisor
-docker stop nimbus; docker rm nimbus
-docker stop zookeeper; docker rm zookeeper
+# Used to stop running storm-docker Docker containers.
+#
+# To stop every storm-docker Docker container, don't supply any args:
+#
+#     ./destroy-storm.sh
+#
+# To stop the `nimbus` and `ui` containers:
+#
+#    ./destroy-storm.sh nimbus ui
+
+function stop_storm_docker {
+  docker stop $1; docker rm $1
+}
+
+if [ $# -eq 0 ]
+then
+  stop_storm_docker ui
+  stop_storm_docker supervisor
+  stop_storm_docker nimbus
+  stop_storm_docker zookeeper
+else
+  for component in "$@"
+  do
+    stop_storm_docker $component
+  done
+fi
