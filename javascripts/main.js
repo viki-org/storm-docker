7457925499a658ce840ed870ccf4fcc0c1713957
Pang Yan Han <yanhan@viki.com>
2014-06-19 12:00:17 +0800

    Switch to Jekyll
    Let's hope this works
    
    Signed-off-by: Pang Yan Han <yanhan@viki.com>

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,54 @@
-var sectionHeight=function(){var e=$(window).height(),t=$("section").css("height","auto");if(t.outerHeight(!0)<e){var i=t.outerHeight(!0)-t.height();t.height(e-i-20)}else t.css("height","auto")};$(window).resize(sectionHeight),$(document).ready(function(){$("section h1, section h2").each(function(){$("nav ul").append("<li class='tag-"+this.nodeName.toLowerCase()+"'><a href='#"+$(this).text().toLowerCase().replace(/ /g,"-").replace(/[^\w-]+/g,"")+"'>"+$(this).text()+"</a></li>"),$(this).attr("id",$(this).text().toLowerCase().replace(/ /g,"-").replace(/[^\w-]+/g,"")),$("nav ul li:first-child a").parent().addClass("active")}),$("nav ul li").on("click","a",function(e){var t=$($(this).attr("href")).offset().top-190;$("html, body").animate({scrollTop:t},400),$("nav ul li a").parent().removeClass("active"),$(this).parent().addClass("active"),e.preventDefault()}),sectionHeight(),$("img").load(sectionHeight)}),fixScale=function(e){function t(){o.content="width=device-width,minimum-scale="+s[0]+",maximum-scale="+s[1],e.removeEventListener(a,t,!0)}var i="addEventListener",a="gesturestart",n="querySelectorAll",s=[1,1],o=n in e?e[n]("meta[name=viewport]"):[];(o=o[o.length-1])&&i in e&&(t(),s=[.25,1.6],e[i](a,t,!0))};
+var sectionHeight = function() {
+  var total    = $(window).height(),
+      $section = $('section').css('height','auto');
+
+  if ($section.outerHeight(true) < total) {
+    var margin = $section.outerHeight(true) - $section.height();
+    $section.height(total - margin - 20);
+  } else {
+    $section.css('height','auto');
+  }
+}
+
+$(window).resize(sectionHeight);
+
+$(document).ready(function(){
+  $("section h1, section h2").each(function(){
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
+    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+    $("nav ul li:first-child a").parent().addClass("active");
+  });
+
+  $("nav ul li").on("click", "a", function(event) {
+    var position = $($(this).attr("href")).offset().top - 190;
+    $("html, body").animate({scrollTop: position}, 400);
+    $("nav ul li a").parent().removeClass("active");
+    $(this).parent().addClass("active");
+    event.preventDefault();
+  });
+
+  sectionHeight();
+
+  $('img').load(sectionHeight);
+});
+
+fixScale = function(doc) {
+
+  var addEvent = 'addEventListener',
+      type = 'gesturestart',
+      qsa = 'querySelectorAll',
+      scales = [1, 1],
+      meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+
+  function fix() {
+    meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
+    doc.removeEventListener(type, fix, true);
+  }
+
+  if ((meta = meta[meta.length - 1]) && addEvent in doc) {
+    fix();
+    scales = [.25, 1.6];
+    doc[addEvent](type, fix, true);
+  }
+
+};
