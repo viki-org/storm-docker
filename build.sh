c4007c9f762cd70c600630da5febd24a51990f4a
Pang Yan Han <yanhan@viki.com>
2014-06-19 12:02:13 +0800

    build.sh - remove _site/build.sh
    Forgot it in the last patch
    
    Signed-off-by: Pang Yan Han <yanhan@viki.com>

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -e
 
 rm -rf _site
 jekyll build
-rm _site/config.rb _site/Gemfile _site/Gemfile.lock _site/Procfile \
-  _site/README.md _site/stylesheets/*.scss
+rm _site/build.sh _site/config.rb _site/Gemfile _site/Gemfile.lock \
+  _site/Procfile _site/README.md _site/stylesheets/*.scss
 rm -rf _site/script
